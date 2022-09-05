4c8119874bff59928395d39b510012515760e4bd
Francisco <85518387+Zobercito@users.noreply.github.com>
2022-09-05 13:24:13 -0500

    Add files via upload

---
+++ clase1/nombre_y_edad/src/main/java/com/mycompany/nombre_y_edad/Nombre_y_edad.java
@@ -0,0 +1,35 @@
+/*
+Clase #1 - 02-09-2022
+
+Programa que pregunte el nombre y edad de una persona y la imprima en pantalla
+
+Para la entrada de edad se usa la clase parseInt
+*/
+
+package com.mycompany.nombre_y_edad;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author franc
+ */
+public class Nombre_y_edad {
+
+    public static void main(String[] args) throws IOException  {
+        String nombre;
+        int edad;
+        
+        BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+        
+        System.out.print("Por favor ingrese su nombre: ");
+        nombre=teclado.readLine();
+        
+        System.out.print("Ingrese su edad: ");
+        edad = Integer.parseInt(teclado.readLine());
+        
+        System.out.println("\nSu nombre es "+nombre+" y su edad es de "+edad+" años.");
+        
+    }
+}
